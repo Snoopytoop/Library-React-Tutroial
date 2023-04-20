36185535da3c1d92d8f5e2ee1035e71f5bba89ed
Snoopytoop <samuelluke1998@gmail.com>
2023-04-20 19:00:56 +0100

    hi

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,26 +1,36 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Highlight from "./UI/Highlight";
 
 const Highlights = () => {
   return (
     <section id="highlights">
-        <div className="container">
-            <div className="row">
-                <h2 className="section__title">
-                    Why choose <span className="purple">Library</span>
-                </h2>
-                <div className="highlight__wrapper">
-                    <div className="highlight">
-                        <div className="higlight__img">
-                            <FontAwesomeIcon icon="bolt" />
-                        </div>
-                        <h3 className="highlight__subtitle">Easy and Quick</h3>
-                        <p className="highlight__para">Get access to the book you purchased online instantly</p>
-                    </div>
-                </div>
-            </div>
+      <div className="container">
+        <div className="row">
+          <h2 className="section__title">
+            Why choose <span className="purple">Library</span>
+          </h2>
+          <div className="highlight__wrapper">
+            <Highlight
+              icon={<FontAwesomeIcon icon="bolt" />}
+              title="Easy and Quick"
+              para="Get access to the book you purchased online instantly"
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon="book-open" />}
+              title="10,000+ Books"
+              para="Library has books in all your favourite categories."
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon="tags" />}
+              title="Affordable"
+              para="Get your hands on popular books for as little as $10."
+            />
+          </div>
         </div>
+      </div>
     </section>
-  )
+  );
 };
 
 export default Highlights;
