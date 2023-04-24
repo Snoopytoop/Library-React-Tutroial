7117fc6b4ab31dd37c866d8c9e729fce400d2b4b
Snoopytoop <samuelluke1998@gmail.com>
2023-04-24 18:01:30 +0100

    more work

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import Landing from "./components/Landing";
 import Nav from "./components/Nav";
-import Highlights from "./components/Highlights";
-import Featured from "./components/Featured";
-import Discounted from "./components/Discounted";
-import Explore from "./components/Explore";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { books  } from "./data"; 
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} /> 
+        <Route path="/books" exact render={() => <Books books={books} />}  /> 
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
